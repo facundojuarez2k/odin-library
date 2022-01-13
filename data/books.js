dc9204065e9519061d38d1b8b4d4e0ee491ca926
Facundo Juarez <facu.j.94@gmail.com>
2022-01-13 14:58:04 -0300

    Add books to the page using js

---
+++ data/books.js
@@ -0,0 +1,40 @@
+export const books = {
+    books: [
+        {
+            title: "IT",
+            author: "Stephen King",
+            pages: 1138,
+            year: 1986,
+            genre: "Horror",
+            pictureUrl: "../img/stephen_king_it.png",
+            description: "IT is the 22nd book published by Stephen King. It was his 18th novel, and the 13th novel written under his own name. The book was released by Viking on September 15, 1986. It was one of the best selling novels in the United States of that year and is now considered a \"modern masterpiece\" of the horror genre in literature."
+        },
+        {
+            title: "1984",
+            author: "George Orwell",
+            pages: 328,
+            year: 1949,
+            genre: "Science Fiction",
+            pictureUrl: "../img/george_orwell_1984.png",
+            description: "Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching..."
+        },
+        {
+            title: "Fahrenheit 451",
+            author: "Ray Bradbury",
+            pages: 249,
+            year: 1953,
+            genre: "Science Fiction",
+            pictureUrl: "../img/ray_bradbury_fahrenheit_451.png",
+            description: "Guy Montag is a fireman. His job is to destroy the most illegal of commodities, the printed book, along with the houses in which they are hidden. Montag never questions the destruction and ruin his actions produce, returning each day to his bland life and wife, Mildred, who spends all day with her television “family.” But when he meets an eccentric young neighbor, Clarisse, who introduces him to a past where people didn’t live in fear and to a present where one sees the world through the ideas in books instead of the mindless chatter of television, Montag begins to question everything he has ever known."
+        },
+        {
+            title: "The Road",
+            author: "Cormac McCarthy",
+            pages: 287,
+            year: 2006,
+            genre: "Novel",
+            pictureUrl: "../img/cormac_mccarthy_the_road.png",
+            description: "A father and his son walk alone through burned America. Nothing moves in the ravaged landscape save the ash on the wind. It is cold enough to crack stones, and when the snow falls it is gray. The sky is dark. Their destination is the coast, although they don't know what, if anything, awaits them there. They have nothing; just a pistol to defend themselves against the lawless bands that stalk the road, the clothes they are wearing, a cart of scavenged food—and each other."
+        }
+    ]
+}
